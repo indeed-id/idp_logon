cbf79651ec9f344ecf4eef71e5c7f0b30e59bd70
Mikhail Baranov <mikhail.baranov@gmail.com>
2014-08-20 00:41:45 +0400

    Карусель для аккаунтов

--- new/js/main.js
+++ new/js/main.js
@@ -1,3 +1,98 @@
-(function ($) {
+(function(window, document, $, undefined) {
+    var carouselDefaults = {
+        container: '.container',
+        controls: {
+            container: '.controls',
+            prev: '.prev',
+            next: '.next',
+            disabledClass: 'disabled'
+        },
+        item: '.item',
+        itemsToShow: 1
+    };
 
-})(jQuery);
+    $.Carousel = function($root, options) {
+        this.$container = $root.find(options.container);
+        this.containerWidth = this.$container.width();
+        this.$controls = $root.find(options.controls);
+        this.$item = $root.find(options.item);
+        this.$prev = $root.find(options.controls.prev);
+        this.$next = $root.find(options.controls.next);
+        this.disabledClass = options.controls.disabledClass;
+        this.step = this.$item.width();
+        this.counter = 0;
+        this.itemsToShow = options.itemsToShow;
+
+        this.init = function() {
+            this._showByIndex(0);
+            this._bind();
+        };
+
+        this._bind = function() {
+            this.$next.on('click', $.proxy(this._next, this));
+            this.$prev.on('click', $.proxy(this._prev, this));
+        };
+
+        this._showByIndex = function(index) {
+            this.$item.animate({
+                left: '-' + this.step * index
+            });
+
+            this.counter = index;
+            this._checkControls();
+        };
+
+        this._next = function(e) {
+            if (this.counter < this.$item.length - this.itemsToShow) {
+                this.counter++;
+                this._showByIndex(this.counter);
+            }
+        };
+
+        this._prev = function(e) {
+            if (this.counter > 0) {
+                this.counter--;
+                this._showByIndex(this.counter);
+            }
+        };
+
+        this._checkControls = function() {
+            this.$prev.removeClass(this.disabledClass);
+            this.$next.removeClass(this.disabledClass);
+
+            if (this.counter === 0) {
+                this.$prev.addClass(this.disabledClass);
+            }
+            if (this.counter === this.$item.length - this.itemsToShow) {
+                this.$next.addClass(this.disabledClass);
+            }
+        };
+    };
+
+    $.fn.carousel = function(options) {
+        return this.each(function() {
+            var $item = $(this);
+            var itemData = $item.data();
+            var instanceOpts = $.extend(
+                {},
+                carouselDefaults,
+                options,
+                itemData
+            );
+
+            return new $.Carousel($item, instanceOpts).init();
+        });
+    };
+
+    $('.account').carousel({
+        container: '.account__users',
+        controls: {
+            container: '.account__control',
+            prev: '.account__arrow_prev',
+            next: '.account__arrow_next',
+            disabledClass: 'account__arrow_disabled'
+        },
+        item: '.account__user',
+        itemsToShow: 2
+    })
+})(window, document, jQuery, undefined);
